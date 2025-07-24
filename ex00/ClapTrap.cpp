4ddbceeed52ee18079b5dd027e3a1d8e24eb8e80
jorvarea <jorvarea@c3r4s6.42malaga.com>
2025-07-24 19:05:43 +0200

    remove unused lib

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,5 +1,4 @@
 #include "ClapTrap.hpp"
-#include <algorithm>
 
 ClapTrap::ClapTrap() : _name("default"), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
     std::cout << "ClapTrap(" << _name << ") has been created" << std::endl;
